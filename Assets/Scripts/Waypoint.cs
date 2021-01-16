9772c3b077f5b26490e1d3cbcba89d4309e38d65
Griffin <60614863+h-griffin@users.noreply.github.com>
2021-01-15 22:29:10 -0800

    first commit

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] private Vector3[] points;
+    private Vector3 _currentPosition;
+    private bool _gameStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameStarted = true;
+        _currentPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!_gameStarted && transform.hasChanged)
+        {
+            _currentPosition = transform.position;
+        }
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(points[i]+ _currentPosition, 0.5f);
+
+            if (i < points.Length - 1)
+            {
+                Gizmos.color = Color.gray;
+                Gizmos.DrawLine(points[i], points[i + 1] + _currentPosition);
+            }
+        }
+    }
+}
